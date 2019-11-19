bfc6f05d1e6e0ec129a24bad9955354ed971e7a2
gabmadera <gabmadera@gmail.com>
2019-11-19 20:26:07 +0100

    Me enredé

---
+++ cadenas.js
@@ -0,0 +1,11 @@
+//Metodo 1
+var friend1 = "Amigo";
+alert(friend1.toLowerCase());
+//Metodo 2
+var friend2 = "Amigo";
+alert(friend2.replace("Ami","ami"));
+//Metodo 3
+var friend3 = "Amigo";
+var a = "a";
+alert(friend3.slice(0,1));
+alert(a+friend3);
